a527d1acbfa16c5d539230165d780edafb9910fb
subbat9710 <teela.subba1991@gmail.com>
2018-04-29 22:03:42 -0400

    The first project of Udacity- Sandwich club

---
+++ sandwich-club-starter-code/app/src/main/java/com/udacity/sandwichclub/MainActivity.java
@@ -0,0 +1,57 @@
+package com.udacity.sandwichclub;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] sandwiches = getResources().getStringArray(R.array.sandwich_names);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, sandwiches){
+
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+                View view = super.getView(position, convertView, parent);
+
+                TextView v = view.findViewById(android.R.id.text1);
+                v.setTextSize(18);
+
+                v.setTypeface( null, Typeface.BOLD);
+                return view;
+            }
+
+        };
+
+        // Simplification: Using a ListView instead of a RecyclerView
+        ListView listView = findViewById(R.id.sandwiches_listview);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                launchDetailActivity(position);
+            }
+        });
+    }
+
+    private void launchDetailActivity(int position) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(DetailActivity.EXTRA_POSITION, position);
+        startActivity(intent);
+    }
+}
